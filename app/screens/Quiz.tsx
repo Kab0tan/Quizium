24c45d98ff1bd61814a72e94e4efa079f657d770
Kabotan <justinleddet@gmail.com>
2024-11-22 12:09:53 +0100

    small type fix

--- app/screens/Quiz.tsx
+++ app/screens/Quiz.tsx
@@ -63,7 +63,7 @@ export default function Quiz() {
     return unsubscribe;
   }, [currentQuestionIndex, allQuestions, navigation]);
 
-  const shuffleArray = (array) => {
+  const shuffleArray = (array: any) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
